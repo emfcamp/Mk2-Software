8ca683de0ee1cafe0d16c7982ee0e31fe0b82a0a
Marek Ventur <marekventur@gmail.com>
2014-08-10 01:03:38 +0100

    Add schedule content updater

---
+++ content_updater/schedule.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import psycopg2
+import logging
+import json
+import requests
+import struct
+import sys
+import binascii
+import time
+import msgpack
+
+config = json.load(open('config.json'))
+
+scheduleJson = json.load(open('../emw-talks.json'))
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('weather');
+
+logger.info("Updating schedule");
+
+stageIds = {
+    "Stage Alpha": 1,
+    "Stage Beta": 2,
+    "Workshop": 3
+}
+
+typeIds = {
+    "talk": 1,
+    "lightning": 2,
+    "workshop": 3
+}
+
+talks = []
+for stage in scheduleJson['stages']:
+    stageName = stage['name']
+    stageId = stageIds[stageName]
+    for event in stage['events']:
+        typeId = typeIds[event['type']]
+        startTimestamp = int(round(time.mktime(time.strptime(event['start'], "%Y-%m-%d %H:%M:%S"))));
+        endTimestamp = int(round(time.mktime(time.strptime(event['end'], "%Y-%m-%d %H:%M:%S"))));
+
+        speaker = "";
+        if 'speaker' in event:
+            speaker = event['speaker']
+
+        talks.append([
+            stageId,
+            typeId,
+            startTimestamp,
+            endTimestamp,
+            speaker,
+            event['title'],
+           # event['description'] Thiss is quite a lot of data. Leaving it out for now
+        ])
+
+packed = msgpack.packb(talks)
+
+logger.info("Content length: %d", len(packed))
+
+# Connect do db
+logger.info("Connection to DB...")
+conn = psycopg2.connect(config['dbConnectionString'])
+cursor = conn.cursor()
+
+# Update content
+logger.info("Upating content...")
+contentAsHexString = binascii.hexlify(packed)
+cursor.execute("UPDATE content SET content = decode(%(content)s, 'hex') WHERE name = 'SCHEDULE_FRIDAY';", {'content': contentAsHexString});
+conn.commit()
+cursor.close()
+conn.close()
+
+logger.info("All done")
