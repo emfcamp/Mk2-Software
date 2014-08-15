4d7479effc9765a27367236b8d0d1e2270d7545d
Marek Ventur <marekventur@gmail.com>
2014-08-16 00:28:12 +0100

    Fix bug in mcp

--- mcp/lib/mcpTcpServer.py
+++ mcp/lib/mcpTcpServer.py
@@ -8,7 +8,7 @@ from tornado.tcpserver import TCPServer
 from tornado import gen
 
 class McpTcpServer(TCPServer):
-    
+
     def __init__(self, config, dataQueue):
         super(McpTcpServer, self).__init__()
         self.config = config
@@ -26,12 +26,12 @@ class McpTcpServer(TCPServer):
 
     def get_unused_channel(self):
         for channel in range(6, 200, 2):
-            for connection in self.connections:
+            for connection in self.connections.itervalues():
                 if connection['mainChannel'] == channel:
                     break
             else:
                 return channel
-    
+
 
     # gen.coroutine was swallowing errors since it's returning a future. This bubbles errors up.
     @gen.engine
@@ -43,7 +43,7 @@ class McpTcpServer(TCPServer):
             port = address[1]
             logger = logging.getLogger('socket-{0}@{1}:{2}'.format(connectionId, ip, port))
             logger.info("Connection opened")
-        
+
             # Initial packet
             line = yield stream.read_until('\n')
             gwInfo = json.loads(line);
@@ -103,7 +103,7 @@ class McpTcpServer(TCPServer):
             stream.close()
             self.remove_connection(connectionId)
             raise
-         
+
 
     def remove_connection(self, connectionId):
         if connectionId in self.connections:
