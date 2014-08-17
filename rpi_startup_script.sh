46f15f655af95a6144bd59f687deff3eabd3fa22
Marek Ventur <marekventur@gmail.com>
2014-08-17 20:37:53 +0100

    Rember when I said I added the init script for the GWs? Yes? Turns out I was lying. Here's the real thing.

--- rpi_startup_script.sh
+++ rpi_startup_script.sh
@@ -2,51 +2,138 @@
 
 ### BEGIN INIT INFO
 # Provides:          tilda-gateway-supervisor
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
+# Required-Start:    $all
+# Required-Stop:     $all
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: Download, run and restart tilda gateway script after exception
-# Description:       Download, run and restart tilda gateway script after exception
+# Short-Description: Download, run and restart tilda gateway script after excsueption
 ### END INIT INFO
 
 # This is the script that gets started after startup. Only this script
-# is needed, nothing else. Make sure that user "tilda" exists! (useradd tilda)
+# is needed, nothing else.
+# Make sure that user "tilda" exists! (useradd tilda) and a folder "/tilda" and "/var/log/tilda"
+# that is owned by tilda. All this can be done by running "setup" on this scipt
 # Copy this script in /etc/init.d/tilda-gateway-supervisor, chmod 755 and execute
 # "sudo update-rc.d tilda-gateway-supervisor defaults" to make it run after boot
 
-software_root="/tildamk2Software"
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
 
-prgcmd="$(basename $0) worker" # What gets executed?
-prgname=tilda-gateway-supervisor # What's the name (used to ensure only one instance is running)
-prguser=tilda # Which user should be used
+prgcmd="$SCRIPTPATH/$(basename $0) worker" # What gets executed?
+prgname=tildagws # What's the name (used to ensure only one instance is running)
+prguser=root # Will be dropped down when the actual script is started
 pidfile=/var/run/tilda-gateway-supervisor.pid # Where should the pid file be stored?
 
-# If you want a command to always run, put it here
+worker() {
+    while :
+    do
+        cd /tilda
 
-# Carry out specific functions when asked to by the system
+        echo "Downloading current version of software from github..."
+        wget https://github.com/emfcamp/Mk2-Software/archive/master.zip -O master.zip -q
+
+        echo "Unpacking code..."
+        rm -rf Mk2-Software-master
+        unzip -q master.zip
+
+        cd Mk2-Software-master
+
+        echo "Run setup..."
+        ./setup.py develop
+
+        echo "Run gateway..."
+        sudo -u tilda ./bin/gateway.py 192.168.1.71
+
+        echo "Process died, start again in 5 sec"
+        sleep 5
+    done
+}
+
+setup() {
+    sudo apt-get install -y python-setuptools
+    useradd tilda
+    usermod -a -G dialout tilda
+    mkdir /tilda
+    mkdir /var/log/tilda
+    chown tilda /tilda
+    chown tilda /var/log/tilda
+}
+
+start() {
+    if [ -f $pidfile ]; then
+        pid=`cat $pidfile`
+        kill -0 $pid >& /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Service has already been started."
+            return 1
+        fi
+    fi
+
+    nohup start-stop-daemon -c $prguser -n $prgname -p $pidfile -m --exec /usr/bin/env --start $prgcmd >>/var/log/tilda/gateway.log 2>&1 &
+
+    if [ $? -eq 0 ]; then
+        echo "Service started."
+        return 0
+    else
+        echo "Failed to start service."
+        return 1
+    fi
+}
+
+stop() {
+
+    if [ ! -f $pidfile ]; then
+        echo "Service not started."
+        return 1
+    fi
+
+    start-stop-daemon -p $pidfile --stop
+    if [ $? -ne 0 ]; then
+        echo "Failed to stop service."
+        return 1
+    fi
+
+    echo -e "\nService stopped."
+    rm $pidfile
+}
+
+status() {
+
+    if [ -f $pidfile ]; then
+        pid=`cat $pidfile`
+        kill -0 $pid >& /dev/null
+        if [ $? -eq 0 ]; then
+            echo "Service running. (PID: ${pid})"
+            return 0
+        else
+            echo "Service might have crashed. (PID: ${pid} file remains)"
+            return 1
+        fi
+    else
+        echo "Service not started."
+        return 0
+    fi
+}
+
+restart() {
+    stop
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+
+    sleep 2
+
+    start
+    return $?
+}
+
 case "$1" in
-    start)
-        echo "Starting tilda-gateway-supervisor"
-        # run application you want to start
-        /etc/init.d/tilda-gateway-supervisor worker
+    start | stop | status | restart | worker | setup)
+        $1
         ;;
-    stop)
-        echo "Stopping tilda-gateway-supervisor"
-        # kill application you want to stop
-        killall tilda-gateway-supervisor
-        ;;
-    worker)
-        while :
-        do
-            echo "Press [CTRL+C] to stop.."
-            sleep 1
-        done
-
     *)
-        echo "Usage: sudo service tilda-gateway-supervisor {start|stop}"
-        exit 1
-        ;;
+        echo "Usage: $0 {start|stop|status|restart|worker|setup}"
+        exit 2
 esac
-
 exit 0
