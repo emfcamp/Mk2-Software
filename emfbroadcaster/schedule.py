a37d689c3f1b1ebdaa3f844aab26da361f6352b1
Richard Jones <rj@metabrew.com>
2014-08-25 15:05:16 +0100

    make schedule broadcaster read from db

--- emfbroadcaster/schedule.py
+++ emfbroadcaster/schedule.py
@@ -2,96 +2,103 @@
 import logging
 import json
 import requests
-import struct
 import sys
-import binascii
-import time
 import tinypacks
-import re
+import psycopg2
+
+
+dbstr = "host='localhost' dbname='schedule' user='tilda' password='tilda'"
+conn = psycopg2.connect(dbstr)
+conn.set_session(autocommit=True)
+cursor = conn.cursor()
+
 
 def show_usage_message():
-    print "Usage: ./schedule.py 2014-08-29 40963"
-    print "Usage: ./schedule.py 2014-08-30 40964"
-    print "Usage: ./schedule.py 2014-08-31 40965"
+    print "Usage: ./schedule.py <fri|sat|sun>"
     sys.exit(1)
 
-if len(sys.argv) < 3:
-    show_usage_message();
 
-day = sys.argv[1];
-if not re.match(r"\d\d\d\d-\d\d-\d\d", day):
+dayname = sys.argv[1]
+
+if dayname == "fri":
+    rid = 40963
+    day = 29
+elif dayname == "sat":
+    rid = 40964
+    day = 30
+elif dayname == "sun":
+    rid = 40965
+    day = 31
+else:
     show_usage_message()
-
-rid = int(sys.argv[2])
 
 config = json.load(open('../etc/config.json'))
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger('schedule');
+logger = logging.getLogger('schedule')
 
-logger.info("Updating schedule for day %s and rid %d", day, rid);
 
-type_ids = {
-    "lecture": 1,
-    "installation": 2,
-    "workshop": 3,
-    "other": 4
-}
+def get_schedule_for_day_and_location(day, locid):
+    sql = "select id, title, type_id, abstract, speaker_names, extract('epoch' from start_time), extract('epoch' from end_time), location_id, location_name, date_part('day', start_time) as day from event where date_part('day', start_time) = %s and location_id = %s"
+    cursor.execute(sql, (day, locid))
+    rows = cursor.fetchall()
+    ret = []
+    for row in rows:
+        ret.append({"id": row[0],
+                    "title": row[1],
+                    "type": row[2],
+                    "abstract": row[3],
+                    "speaker_names": row[4],
+                    "start_time": int(row[5]),
+                    "end_time": int(row[6]),
+                    "location_id": row[7],
+                    })
+    return ret
 
-resp = requests.get(url=config['scheduleUrl'])
-schedule_json = json.loads(resp.text);
 
-print schedule_json;
-
-talks = []
-for event in schedule_json['conference_events']['events']:
-
-    if 'start_time' in event:
-        location_id = event['room']['id']
-        start_timestamp = int(round(time.mktime(time.strptime(event['start_time'], "%Y-%m-%dT%H:%M:%SZ"))));
-        end_timestamp = int(round(time.mktime(time.strptime(event['end_time'], "%Y-%m-%dT%H:%M:%SZ"))));
-        matches_day = (event['start_time'][0:10] == day)
+def type_name_to_id(t):
+    types = {
+        "lecture": 1,
+        "installation": 2,
+        "workshop": 3,
+        "other": 4
+    }
+    if t in types:
+        return types[t]
     else:
-        location_id = 0
-        start_timestamp = 0
-        end_timestamp = 0
-        matches_day = False
-        logger.info("Talk without start_time: %s", event)
+        return 4
 
-    type_id = type_ids[event['type']]
 
-    speaker = "";
-    if 'speaker' in event:
-        speaker = event['speaker']['full_public_name']
+def pack_location_day_data(loc, s):
+    packed = tinypacks.pack(len(s))
+    for event in s:
+        start_timestamp = event['start_time']
+        end_timestamp = event['end_time']
+        type_id = type_name_to_id(event['type'])
+        speaker = ""
+        if 'speaker' in event:
+            speaker = event['speaker']['full_public_name']
+        packed += tinypacks.pack(event['location_id'])
+        packed += tinypacks.pack(type_id)
+        packed += tinypacks.pack(start_timestamp)
+        packed += tinypacks.pack(end_timestamp)
+        packed += tinypacks.pack(speaker)
+        packed += tinypacks.pack(event['title'])
+    return packed
 
-    if matches_day:
-        talks.append({
-            "location_id": location_id,
-            "type_id": type_id,
-            "start_timestamp": start_timestamp,
-            "end_timestamp": end_timestamp,
-            "speaker": speaker,
-            "title": event['title'],
-            "abstract": event['abstract']
-        })
 
-packed = tinypacks.pack(len(talks));
-for talk in talks:
-    packed += tinypacks.pack(talk['location_id']);
-    packed += tinypacks.pack(talk['type_id']);
-    packed += tinypacks.pack(talk['start_timestamp']);
-    packed += tinypacks.pack(talk['end_timestamp']);
-    packed += tinypacks.pack(talk['speaker']);
-    packed += tinypacks.pack(talk['title']);
+cursor.execute("select distinct location_id from event where location_id is not null order by location_id")
+locids = cursor.fetchall()
 
-print binascii.hexlify(packed)
-logger.info("Content length: %d", len(packed))
-
-# Update content
-logger.info("Sending content...")
-params = dict( rid=rid )
-response = requests.post(url=config['mcpBroadcastEndpoint'], params=params, data=packed)
-
-logger.info("Response from MCP: %s", response)
-
-logger.info("All done")
+for locidtup in locids:
+    loc = locidtup[0]
+    s = get_schedule_for_day_and_location(day, loc)
+    print s
+    p = pack_location_day_data(loc, s)
+    params = dict(rid=rid)
+    logger.info("Uploading location %d...", loc)
+    response = requests.post(url=config['mcpBroadcastEndpoint'], params=params, data=p)
+    if (response.status_code == 200):
+        logger.info("OK")
+    else:
+        logger.warn("ERROR!!!!! %s", response)
